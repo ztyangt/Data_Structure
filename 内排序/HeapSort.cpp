52bfdc5cf997d780ca06dc53302a3cbe1ebdeea9
root <yang2210670@163.com>
2022-12-03 19:49:52 +0800

    堆排序(待完成)

---
+++ 内排序/HeapSort.cpp
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+typedef int ElemType;
+
+typedef struct {
+    ElemType *elem;
+    int TableLen;
+}SSTable;
+
+void InitSST(SSTable &ST,int len){
+    ST.TableLen = len;
+    ST.elem = (ElemType *)malloc(sizeof(SSTable)*ST.TableLen);
+    srand(time(NULL));
+    for(int i=0;i<ST.TableLen;i++){
+        ST.elem[i] = rand()%100;
+    }
+}
+
+// 建立大根堆
+void BuildMaxHeap(SSTable &ST){
+    int i,j,k,temp;
+    for(i= ST.TableLen/2;i>0;i--){
+        k=i;
+        temp = ST.elem[k];
+        for(j=2*k;j<=ST.TableLen;j*=2){
+            if(j<ST.TableLen && ST.elem[j+1]) j++;
+            if(ST.elem[k]>ST.elem[j]) break;
+            else{
+                ST.elem[k] = ST.elem[j];
+                k=j;
+            }
+        }
+        ST.elem[k] = temp;
+    }
+}
+
+void HeapSort(SSTable &ST){
+  int i,j,min,temp;
+  for(i=0;i<ST.TableLen-1;i++){
+    min = i;
+    for(j=i+1;j<ST.TableLen;j++){
+        if(ST.elem[j] < ST.elem[min]) min=j;
+    }
+    if(i!=min){
+        temp = ST.elem[i];
+        ST.elem[i]=ST.elem[min];
+        ST.elem[min] = temp;
+    }
+  }
+} 
+
+
+
+void ST_Print(SSTable ST){
+    for(int i = 0;i<ST.TableLen;i++){
+        printf("%d\t",ST.elem[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    SSTable ST;
+    InitSST(ST,10);
+    printf("排序前数组为：");
+    ST_Print(ST);
+    HeapSort(ST);
+    printf("排序后数组为：");
+    ST_Print(ST);
+    return 0; 
+}
